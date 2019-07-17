3c03243401977d9b0799c1ec40db104d85643549
su.sung <su.sung@foxmail.com>
2019-07-17 23:55:25 +0800

    update

--- clean/clean.go
+++ clean/clean.go
@@ -8,14 +8,14 @@ import (
 	"net/http"
 	"path/filepath"
 
-	"github.com/qor/auth"
-	"github.com/qor/auth/claims"
-	"github.com/qor/auth/providers/password"
-	"github.com/qor/i18n"
-	"github.com/qor/i18n/backends/yaml"
-	"github.com/qor/qor"
-	"github.com/qor/qor/utils"
-	"github.com/qor/render"
+	"github.com/kisrobot/auth"
+	"github.com/kisrobot/auth/claims"
+	"github.com/kisrobot/auth/providers/password"
+	"github.com/kisrobot/i18n"
+	"github.com/kisrobot/i18n/backends/yaml"
+	"github.com/kisrobot/qor"
+	"github.com/kisrobot/qor/utils"
+	"github.com/kisrobot/render"
 )
 
 // ErrPasswordConfirmationNotMatch password confirmation not match error
@@ -26,7 +26,7 @@ func New(config *auth.Config) *auth.Auth {
 	if config == nil {
 		config = &auth.Config{}
 	}
-	config.ViewPaths = append(config.ViewPaths, "github.com/qor/auth_themes/clean/views")
+	config.ViewPaths = append(config.ViewPaths, "github.com/kisrobot/auth_themes/clean/views")
 
 	if config.DB == nil {
 		fmt.Print("Please configure *gorm.DB for Auth theme clean")
@@ -36,7 +36,7 @@ func New(config *auth.Config) *auth.Auth {
 		yamlBackend := yaml.New()
 		I18n := i18n.New(yamlBackend)
 		for _, gopath := range append([]string{filepath.Join(utils.AppRoot, "vendor")}, utils.GOPATH()...) {
-			filePath := filepath.Join(gopath, "src", "github.com/qor/auth_themes/clean/locales/en-US.yml")
+			filePath := filepath.Join(gopath, "src", "github.com/kisrobot/auth_themes/clean/locales/en-US.yml")
 			if content, err := ioutil.ReadFile(filePath); err == nil {
 				translations, _ := yamlBackend.LoadYAMLContent(content)
 				for _, translation := range translations {
